95e40e924ca4b3fdeaa2d35059f55f560a89bf33
Sulochana1409 <sulochana.syb@gmail.com>
2024-10-01 20:31:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-
 from snowflake.snowpark.functions import col 
 import requests 
 
@@ -18,6 +17,7 @@ cnx=st.connection("snowflake")
 session =cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select (col('FRUIT_NAME'),col('SEARCH_ON'))
+
 pd_df=my_dataframe.to_pandas()
 #st.dataframe(pd_df)
 #st.stop()
@@ -28,15 +28,20 @@ ingredients_list = st.multiselect(
    , my_dataframe
    , max_selections=5
 )
+
 if ingredients_list:
    ingredients_string = ''
     
-   for fruit_chosen in ingredients_list: 
+   for fruit_chosen in ingredients_list:
+       
            ingredients_string += fruit_chosen + ' '
+    
            search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-           st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+           st.write('The search value for ', fruit_chosen,' is ', search_on, '.')   
+
            st.subheader(fruit_chosen + 'Nutrition Information')     
-           fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_chosen)
+        
+           fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_chosen)
         
            fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
       
@@ -49,8 +54,8 @@ if ingredients_list:
 
            if time_to_insert :
     
-                session.sql(my_insert_stmt).collect()
+               session.sql(my_insert_stmt).collect()
     
-                st.success('Your Smoothie is ordered!', icon="✅")
+               st.success('Your Smoothie is ordered!', icon="✅")
 
 
